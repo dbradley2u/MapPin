3a741a2e8bb65c79c87751a12975f2599e00c640
dbradley2u <porenn4u@yahoo.com>
2016-03-08 21:14:25 -0500

    Changes to add MyPin subclass, duplicate target MyPinSatellite, update ViewController

---
+++ MapPin/MyPin.swift
@@ -0,0 +1,21 @@
+//
+//  MyPin.swift
+//  MapPin
+//
+//  Created by Denise Bradley on 3/8/16.
+//  Copyright © 2016 Denise Bradley. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MyPin: MKPointAnnotation {
+    
+    init(title : String, subtitle : String, coordinate : CLLocationCoordinate2D) {
+        super.init()
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+
+}
--- MapPin/ViewController.swift
+++ MapPin/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var currentBundle : String = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleIdentifier") as! String
+        print(currentBundle)
+        
         mapView.delegate = self
+        
+        if currentBundle.hasSuffix("MapPinSatellite")
+        {
+            mapView.mapType = MKMapType.Satellite
+        }
         
         let centerPoint = CLLocationCoordinate2D(latitude: 52.011937, longitude: -3.713379)
         let coordinateSpan = MKCoordinateSpanMake(3.5, 3.5)
@@ -27,30 +35,25 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.setRegion(coordinateRegion, animated: false)
         mapView.regionThatFits(coordinateRegion)
         
-        var annotation1 = MKPointAnnotation()
-        annotation1.title = "Swansea Bay"
-        annotation1.subtitle = "Beautiful Beaches"
-        annotation1.coordinate = CLLocationCoordinate2DMake(51.587736, -3.90152)
+        var annotation1 = MyPin(title: "Swansea Bay",
+            subtitle: "Beautiful Beaches",
+            coordinate: CLLocationCoordinate2DMake(51.587736, -3.90152))
         
-        var annotation2 = MKPointAnnotation()
-        annotation2.title = "Menai Bridge"
-        annotation2.subtitle = "Fantastic Engineering"
-        annotation2.coordinate = CLLocationCoordinate2DMake(53.220527, -4.163562)
+        var annotation2 = MyPin(title: "Menai Bridge",
+            subtitle : "Fantastic Engineering",
+            coordinate : CLLocationCoordinate2DMake(53.220527, -4.163562))
         
-        var annotation3 = MKPointAnnotation()
-        annotation3.title = "Parc Y Scarlets"
-        annotation3.subtitle = "Oh Dear"
-        annotation3.coordinate = CLLocationCoordinate2DMake(51.678809, -4.127469)
+        var annotation3 = MyPin(title: "Parc Y Scarlets",
+            subtitle : "Oh Dear",
+            coordinate : CLLocationCoordinate2DMake(51.678809, -4.127469))
         
-        var annotation4 = MKPointAnnotation()
-        annotation4.title = "Castell Coch"
-        annotation4.subtitle = "A Fairytale Castle"
-        annotation4.coordinate = CLLocationCoordinate2DMake(51.535819, -3.2547)
+        var annotation4 = MyPin(title: "Castell Coch",
+            subtitle : "A Fairytale Castle",
+            coordinate : CLLocationCoordinate2DMake(51.535819, -3.2547))
         
-        var annotation5 = MKPointAnnotation()
-        annotation5.title = "Arthur's Stone"
-        annotation5.subtitle = "Rock of Legend"
-        annotation5.coordinate = CLLocationCoordinate2DMake(51.593735, -4.179525)
+        var annotation5 = MyPin(title: "Arthur's Stone",
+            subtitle : "Rock of Legend",
+            coordinate : CLLocationCoordinate2DMake(51.593735, -4.179525))
         
         mapView.addAnnotation(annotation1)
         mapView.addAnnotation(annotation2)
